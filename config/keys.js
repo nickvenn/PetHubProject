9a063186e2853a133fd04e7ccdab23e5ddea1df3
Nick Vennarucci <nickvenn@comcast.net>
2019-03-28 15:41:17 -0700

    push

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+
 module.exports = {
-  mongoURI: `mongodb://`+ process.env.NAME + `:` + process.env.PASSWORD + `@ds113522.mlab.com:13522/petdatabase` 
-};
+    mongoURI: `mongodb://` + process.env.NAME + `:` + process.env.PASSWORD + `@ds113522.mlab.com:13522/petdatabase`,
+    secretOrKey: "secret"
+  };
--- models/User.js
+++ models/User.js
@@ -19,4 +19,5 @@ const UserSchema = new Schema({
     default: Date.now
   }
 });
+
 module.exports = User = mongoose.model("users", UserSchema);
