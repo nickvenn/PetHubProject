4d73b9c945db6aaadebbac38c762925a0211f768
Nick Vennarucci <nickvenn@comcast.net>
2019-04-02 21:21:16 -0700

    changes

--- client/src/components/dashboard/Map.js
+++ client/src/components/dashboard/Map.js
@@ -1,50 +1,26 @@
 // "AIzaSyC8NiCJndcIn85_FDZyrsNSCwKpXYEddCY"
 
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import React from "react"
+import ReactGoogleMapLoader from "react-google-maps-loader"
 
-class SimpleMap extends Component {
+const App = () =>
+  <ReactGoogleMapLoader
+    params={{
+        key: "AIzaSyC8NiCJndcIn85_FDZyrsNSCwKpXYEddCY", // Define your api key here
+        libraries: "places,geometry", // To request multiple libraries, separate them with a comma
+    }}
+    render={(googleMaps, error) =>
+        googleMaps ? (
+            <div>
+                {/*Show a custom error if SDK Authentication Error. See N/B 2 below.*/}
+                {error ? error : "Google Maps is loaded !"}
+            </div>
+        )   :   (
+            <div>
+                {/*Check for network error so loading state ends if user lost connection.*/}
+                {error === "Network Error" ? <p>{error}</p> : <p>isLoading...</p>}
+            </div>
+        )
+    }/>
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      center: {
-        lat: 59.955413,
-        lng:30.337844
-      },
-      zoom: 11
-    }
-  }
- 
-  componentDidMount = () => {
-    console.log(this.props.lat + " " + this.props.lng);
-    this.setState({
-      lat: this.props.lat,
-      lng: this.props.lng
-    })
-  }
-
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyC8NiCJndcIn85_FDZyrsNSCwKpXYEddCY" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-
-};
- 
-export default SimpleMap;
+    export default App;
