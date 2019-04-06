de481d24e742945ab990843c1db68c62558c8ce4
Nick Vennarucci <nickvenn@comcast.net>
2019-04-05 23:18:41 -0700

    new push

--- server.js
+++ server.js
@@ -31,6 +31,17 @@ app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 const MONGODB_URI = db || 'mongodb://localhost/test';
 mongoose.connect(MONGODB_URI).then(() => console.log("heroku mongodb connected! " + MONGODB_URI));
