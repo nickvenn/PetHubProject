fffaa1129ab449233c0704c6ba7cd379a7c402f6
Nick Vennarucci <nickvenn@comcast.net>
2019-04-06 07:23:07 -0700

    changes

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    mongoURI: `mongodb://` + process.env.NAME + `:` + process.env.PASSWORD + `@ds113522.mlab.com:13522/petdatabase`,
+  MONGODB_URI: `mongodb://` + process.env.NAME + `:` + process.env.PASSWORD + `@ds113522.mlab.com:13522/petdatabase`,
     secretOrKey: "secret"
   }
--- server.js
+++ server.js
@@ -17,27 +17,24 @@ app.use(bodyParser.json());
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
 }
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-// DB Config
-const db = require("./config/keys").mongoURI;
-// Connect to MongoDB
-mongoose.connect(db,{useNewUrlParser: true} || "mongodb://user:United32@ds139844.mlab.com:39844/heroku_3vzql241")
-.then(() => console.log("MONGODB CONNECTED")).catch(err => console.log(err));
-// mongoose
-//   .connect(
-//     db,
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => console.log("MongoDB successfully connected"))
-//   .catch(err => console.log(err));
-// Passport middleware
+
+
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+// DB Config
+const db = require("./config/keys").MONGODB_URI;
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:United32@ds139844.mlab.com:39844/heroku_3vzql241",
+{
+    useCreateIndex: true,
+    useNewUrlParser: true
+}
+).then(() => console.log("MONGODB CONNECTED")).catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
